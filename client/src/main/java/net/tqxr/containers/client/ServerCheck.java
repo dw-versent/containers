e13395bdc2be5b95223c178a35b3ccaa7e84691d
wbit <whatbirdisthat@gmail.com>
2016-11-09 01:27:06 +1100

    nice little network check util

--- client/src/main/java/net/tqxr/containers/client/ServerCheck.java
+++ client/src/main/java/net/tqxr/containers/client/ServerCheck.java
@@ -1,7 +1,24 @@
 package net.tqxr.containers.client;
 
-/**
- * Created by david on 9/11/16.
- */
-public class ServerCheck {
+import java.net.Socket;
+
+class ServerCheck {
+
+    // thanks to Svish:
+    // http://www.geekality.net/2013/04/30/java-simple-check-to-see-if-a-server-is-listening-on-a-port/
+    static boolean serverListening(String host, int port) {
+        Socket s = null;
+        try {
+            s = new Socket(host, port);
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            if (s != null)
+                try {
+                    s.close();
+                } catch (Exception ignored) {
+                }
+        }
+    }
 }
